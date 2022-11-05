bd06c042bef27da4d6fb6cfe59e27c1fb0238e75
Nurtau <nurtau.toganbay@targetai.kz>
2022-11-05 19:56:15 +0600

    ui/improve ui of admin sidebar

--- src/components/molecules/AdminSidebar.tsx
+++ src/components/molecules/AdminSidebar.tsx
@@ -1,17 +1,38 @@
-import { Drawer, List, Divider, ListItem, ListItemText } from "@mui/material";
+import { useLocation, Link } from "react-router-dom";
 
-import { LogoName } from "@app/components";
+import { Box,Drawer, List, Divider, ListItem, ListItemText, ListItemButton, ListItemIcon } from "@mui/material";
+import { PeopleAltOutlined, BadgeOutlined, BusinessOutlined, MedicalServicesOutlined } from "@mui/icons-material";
+
+import { Logo } from "@app/ui";
 
 const SIDEBAR_SECTIONS = [
-  "Patients",
-  "Doctors",
-  "Departments",
-  "Specializations",
+  {
+    text: "Patients",
+    icon: <PeopleAltOutlined />,
+    path: "/admin/patients",
+  },
+{
+    text: "Doctors",
+    icon: <BadgeOutlined />,
+    path: "/admin/doctors",
+  },
+{
+    text: "Departments",
+    icon: <BusinessOutlined />,
+    path: "/admin/departments",
+  },
+{
+    text: "Specializations",
+    icon: <MedicalServicesOutlined />,
+    path: "/admin/specializations",
+  },
 ] as const;
 
 const SIDEBAR_WIDTH = 240;
 
 export const AdminSidebar = () => {
+  const location = useLocation();
+
   return (
     <Drawer
       sx={{
@@ -25,12 +46,22 @@ export const AdminSidebar = () => {
       variant="permanent"
       anchor="left"
     >
-      <LogoName />
+      <Box sx={{py: "20px", mx: "auto"}}>
+      <Logo />
+      </Box>
       <Divider />
       <List>
-        {SIDEBAR_SECTIONS.map((section, index) => (
+        {SIDEBAR_SECTIONS.map(({text, icon, path}, index) => (
           <ListItem key={index} disablePadding>
-            <ListItemText primary={section} />
+          <Link to={path} style={{width: "100%"}}>
+          <ListItemButton selected={location.pathname === path} component="div">
+            <ListItemIcon color="#1F2AD6">
+              {icon}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+            </ListItemButton>
+
+            </Link>
           </ListItem>
         ))}
       </List>
